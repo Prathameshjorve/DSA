598886c66d42a768107ac1deb4258971392944c6
Prathamesh__ <prathameshjorve09@gmail.com>
2026-07-15 20:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (40.47%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+
+        //first find blue gola index
+        int gola_index= -1 ;
+
+        for (int i =n-1 ; i > 0 ;i--){
+
+            if(nums[i]> nums[i-1]){
+                gola_index = i-1;
+                break;
+
+            }
+        }
+
+        if(gola_index != -1){
+        int swap_index = gola_index;
+
+        for(int j = n-1; j >= gola_index+1 ; j-- ){//right side check +1 for the greater number
+            if(nums[j] > nums[gola_index]){
+
+                swap_index = j;
+                break;
+            }
+        }
+        swap(nums[gola_index], nums[swap_index]);
+        }
+        reverse(nums.begin() + gola_index + 1 , nums.end());
+
+        
+    }
+
+    
+};
