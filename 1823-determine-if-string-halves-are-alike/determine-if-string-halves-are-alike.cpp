2cd7f396685224e4b4402b06c2dba9f4e5acba35
Prathamesh__ <prathameshjorve09@gmail.com>
2026-08-03 12:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (30.20%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool isVowel(char &ch){
-        return(ch=='a' ||  ch =='e'||ch=='i'||  ch =='o'||ch=='u'||
-        ch=='A' ||  ch =='E'||ch=='I'||  ch =='O'||ch=='U');
-    }
+    // bool isVowel(char &ch){
+    //     return(ch=='a' ||  ch =='e'||ch=='i'||  ch =='o'||ch=='u'||
+    //     ch=='A' ||  ch =='E'||ch=='I'||  ch =='O'||ch=='U');
+    // }
     bool halvesAreAlike(string s) {
         int n = s.length();
         int mid=n/2;
@@ -13,17 +13,41 @@ public:
         int i=0;
         int j=mid;
 
+        unordered_set<char> st;
+        st.insert('a');
+        st.insert('e');
+        st.insert('i');
+        st.insert('o');
+        st.insert('u');
+        st.insert('A');
+        st.insert('E');
+        st.insert('I');
+        st.insert('O');
+        st.insert('U');
+
         while(i < n/2 && j < n){
 
-            if(isVowel(s[i]))
+            if(st.find(s[i]) !=st.end())
             countL++;
 
-            if(isVowel(s[j]))
+            if(st.find(s[j])!=st.end())
             countR++;
             i++;
             j++;
 
         }
+
+        // while(i < n/2 && j < n){
+
+        //     if(isVowel(s[i]))
+        //     countL++;
+
+        //     if(isVowel(s[j]))
+        //     countR++;
+        //     i++;
+        //     j++;
+
+        // }
         return countL==countR;
 
 
